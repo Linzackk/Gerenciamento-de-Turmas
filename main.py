ebff9759eba293ec5995dbd113615a8a42060526
Isaac de Medeiros <isaac2005medeiros@gmail.com>
2025-08-28 16:06:26 -0300

    Criação de Importação dos dados salvos e Gravação dos dados novos.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # Metódos da Turma: Adicionar_aluno(aluno), media_turma(), aluno_maior_media()
 import json
 
+# Garante que a lista da Turma é sempre criada
 try:
     a = open("Turma.json", "x")
 except FileExistsError:
@@ -23,23 +24,43 @@ class Aluno:
         
 def aluno_para_dict(aluno):
    return {
-        "nome" : aluno.nome,
-        "idade": aluno.idade,
-        "notas": aluno.notas
-    }
+            "nome" : aluno.nome,
+            "idade": aluno.idade,
+            "notas": aluno.notas
+        }
+   
+# Importa a turma salva no arquivo
+with open("Turma.json", "r") as arquivo:
+     turma_salva = json.loads("\n".join(arquivo.readlines()))
     
-# Criando aluno
 print(f"Controle de Turmas")
 
-nome = "Isaac"
-idade = 20
+# Informações Básicas do Aluno
+nome = input("Nome do Aluno: ")
+idade = int(input("Idade do Aluno: "))
+usuario = nome.split()[0] + str(idade)
 
+# Criando aluno
 aluno = Aluno(nome, idade)
-aluno.atribuir_nota("bimestre1", 7.5)
-aluno.atribuir_nota("bimestre2", 8)
 
-dados_aluno = aluno_para_dict(aluno)
+# Atribuindo notas do aluno
+atribuicao = input("Deseja atribuir notas para o aluno? [s/n]").lower().strip()
+if atribuicao == "s":
+    print(f"Insira '-1' caso não queira atribuir uma nota.")
+    bim1 = float(input("Nota para o Bimestre 1: "))
+    bim2 = float(input("Nota para o Bimestre 2: "))
+    
+    # Verifica se a nota é válida (entre 0 e 10) e atribui ao aluno adicionado.
+    if 0 <= bim2 and bim2 <= 10:
+        aluno.atribuir_nota("bimestre1", bim1)
+    if 0 <= bim2 and bim2 <= 10:
+        aluno.atribuir_nota("bimestre2", bim2)
+        
+    print(f"Notas atualizadas com sucesso.")    
+    
+turma_salva[usuario] = aluno_para_dict(aluno)
+
 
 # Salvar no Json
-with open('Turma.json',"a") as arquivo:
-    json.dump(dados_aluno, arquivo, indent=4)
+with open('Turma.json',"w") as arquivo:
+    json.dump(turma_salva, arquivo, indent=4)
